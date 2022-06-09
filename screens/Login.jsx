8c9e75118887d703d15012a29d3fe92f042d8d23
Jose Rafael Soares Borges <a39041@alunos.ipb.pt>
2022-06-09 15:44:25 +0100

    start develop login Screen

---
+++ screens/Login.jsx
@@ -0,0 +1,16 @@
+import React,{useState, useCallback, useContext} from "react";
+import 'react-native-gesture-handler';
+import {View, Text} from "react-native";
+import { IonIcon, IonTitle } from "@ionic/react";
+
+import styles from "../styles/login";
+
+const Login = ()=>{
+    return ( 
+        <View>
+            
+        </View>
+    );
+}
+
+export default Login;
---
+++ styles/login.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+    },
+    header:{
+
+    },
+    bodyInput:{
+
+    },
+    footer:{
+
+    }
+});
+
+export default styles; 
